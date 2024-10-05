0581059cd8e07ed3136f45bccc46320200e62863
tejasvi <tejasvimurthy09@gmail.com>
2024-10-05 13:20:30 +0530

    4th com

---
+++ DemoCLass1.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+public class DemoCLass1 {
+	public static void main(String arr[]) throws Exception{
+		String url = "jdbc:mysql://localhost:3306/school";
+		String usrName = "root";
+		String paws = "Teja@2004";
+		String querry = "INSERT INTO STUDENT VALUES (?, ?, ?)";
+		Connection con = DriverManager.getConnection(url, usrName, paws);
+		PreparedStatement st = con.prepareStatement(querry);
+		st.setInt(1, 6);
+		st.setString(2, "Bhima");
+		st.setInt(3, 12);
+		
+		int count = st.executeUpdate();
+		System.out.println(count);
+		
+		st.close();
+		con.close();
+	}
+}
